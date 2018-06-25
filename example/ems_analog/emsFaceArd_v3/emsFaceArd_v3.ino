cf9a8c6310d34f90c1aa747ce016305f7d6d9d1e
Akira Matsuda <akira.matsuda@me.com>
2018-06-25 23:14:36 +0900

    Fix the issue that could not drive any channel in example/ems_analog/emsFaceArd_v3.ino.

--- example/ems_analog/emsFaceArd_v3/emsFaceArd_v3.ino
+++ example/ems_analog/emsFaceArd_v3/emsFaceArd_v3.ino
@@ -7,7 +7,7 @@
 #include <vector>
 using namespace std;
 
-std::vector<rkmtlab::MultiEMS::Channel> channels;
+std::vector<rkmtlab::MultiEMS::Channel *> channels;
 int channelNum = 4;
 
 void setup()
@@ -15,24 +15,24 @@ void setup()
 	Serial.begin(115200);
 
 	for (int i = 0; i < channelNum; i++) {
-		channels.push_back(rkmtlab::MultiEMS::Channel(i, 100, 100, 50));
+		channels.push_back(new rkmtlab::MultiEMS::Channel(i, 100, 100, 50));
 	}
 }
 
 void loop()
 {
 	//always discharge
-	for (rkmtlab::MultiEMS::Channel c : channels) {
-		c.discharge();
+	for (rkmtlab::MultiEMS::Channel *c : channels) {
+		c->discharge();
 	}
 
 	if (Serial.available() > 19) {
-		for (rkmtlab::MultiEMS::Channel c : channels) {
-			c.pulse = Serial.read();
-			c.frequency = Serial.read();
-			c.voltage = Serial.read();
-			c.state = Serial.read();
-			c.duration = Serial.read() * 100; // converted for msec
+		for (rkmtlab::MultiEMS::Channel *c : channels) {
+			c->pulse = Serial.read();
+			c->frequency = Serial.read();
+			c->voltage = Serial.read();
+			c->state = Serial.read();
+			c->duration = Serial.read() * 100; // converted for msec
 		}
 		channelWrite();
 	}
@@ -43,12 +43,12 @@ void channelWrite()
 {
 	//duration * 10 for debug
 	//this must be updated for individual controls of durations
-	for (int i = 0; i < ((channels[0].duration)) / (1000 / channels[0].frequency); i++) {
-		for (rkmtlab::MultiEMS::Channel c : channels) {
-			c.drive();
+	for (int i = 0; i < ((channels[0]->duration)) / (1000 / channels[0]->frequency); i++) {
+		for (rkmtlab::MultiEMS::Channel *c : channels) {
+			c->drive();
 		}
 		//one pulse relevant for the freq
 		//microsec calculation
-		delayMicroseconds((1000000 / channels[0].frequency) - (rkmtlab::MultiEMS::Channel::Delay * channels.size()));
+		delayMicroseconds((1000000 / channels[0]->frequency) - (rkmtlab::MultiEMS::Channel::Delay * channels.size()));
 	}
 }
